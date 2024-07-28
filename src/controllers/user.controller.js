c98ce6f612ef6dce81bac670b76ab31a402f1f60
prashant7867 <prashantkesarwani7867@gmail.com>
2024-07-28 17:03:21 +0530

    error fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -215,7 +215,7 @@ const changeCurrentPassword=asyncHandler(async(req,res)=>{
 const getCurrentUser=asyncHandler(async(req,res)=>{
      return res
      .status(200)
-     .json(200,req.user,"current user fetced su ccessfully")
+     .json(new ApiResponse(200,req.user,"current user fetced successfully"))
  })
 
 const updateAccountDetails= asyncHandler(async(req,res)=>{
@@ -223,7 +223,7 @@ const updateAccountDetails= asyncHandler(async(req,res)=>{
     if(!(fullName || email)){
       throw new ApiError(400,"all fields are required")
     }
-     const user=User.findByIdAndUpdate(
+     const user=await User.findByIdAndUpdate(
       req.user?._id,
       {
            $set:{
