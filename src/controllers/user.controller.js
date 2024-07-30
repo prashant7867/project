873017da566cc46a8bba2e62c0a1b9bf0c320093
prashant7867 <prashantkesarwani7867@gmail.com>
2024-07-30 22:17:43 +0530

    pipelines added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -299,7 +299,75 @@ const updateUserCoverImage=asyncHandler(async(req,res)=>{
     )
 })
 
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+      const {username}= req.params
+      if(!username?.trim()){
+         throw new ApiError(400,"username not found")
+      }
+      const channel =await User.aggregate([
+         {
+            $match:{
+               username:username?.toLowercase()
+            }
+         },
+         {
+            $lookup:{
+               from:"subcriptions",
+               localField: "_id",
+               foreignField:"channel",
+               as:"subscribers"
+            }
+         },
+         {
+            $lookup:{
+               from:"subcriptions",
+               localField: "_id",
+               foreignField:"subscriber",
+               as:"subscribedTo"
+            }
+         },
+         {
+            $addFields:{
+               subscribersCount:{
+                  $size:"$subscribers"
+               },
+               channelsSubscribedToCount:{
+                  $size:"$subscribedTo"
+               },
+               isSubscribed:{
+                   $cond:{
+                     if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                     then:true,
+                     else:false
+                   }
+               }
+            }
+         },
+         {
+            $project:{
+               fullName:1,
+               username:1,
+               subscribersCount:1,
+               channelsSubscribedToCount:1,
+               isSubscribed:1,
+               avatar:1,
+               coverImage:1,
+               email:1
 
+            }
+         }
+
+      ])
+       
+})
+if(!channel?.length){
+   throw new ApiError(400,"channel doesnt exists")
+}
+return res
+.status(200)
+.json(
+   new ApiResponse(200,channel[0],"User channel fetched successfully")
+)
 
 
 export {
@@ -311,5 +379,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 }
