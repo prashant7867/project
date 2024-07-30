979da089f705fb5b026a244a6fcd6296b9d9149f
prashant7867 <prashantkesarwani7867@gmail.com>
2024-07-30 10:05:28 +0530

    EDIT IN SUBCRIPTION

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -1,7 +1,6 @@
 import mongoose , {Schema} from "mongoose";
 
  const subscriptionSchema= new Schema({
-
     subscriber:{
         type:Schema.Types.ObjectId,//one who is subcribing
         ref:"User"
@@ -10,7 +9,6 @@ import mongoose , {Schema} from "mongoose";
         type:Schema.Types.ObjectId,//one who is subcriber is subscribing
         ref:"User"
     }
-
  },{timestamps:true})
 
  export const Subcription=mongoose.model("Subcription",subscriptionSchema)
